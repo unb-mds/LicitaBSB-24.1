738aaafc529155a3b6dd9aa73c17bc36e6330950
Maria <marianenacarvalho@gmail.com>
2024-04-23 23:10:22 -0300

    fix: removing unused files #38

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from './app.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
 )
