b2399253db02dd54bf2760ee1768325d185e9a57
Thales Euflauzino <thaleseuflauzino@gmail.com>
2024-04-27 21:53:15 -0300

    removendo pasta desnecessarias

---
+++ backend/data_collection/gazette/spiders/base/doem.py
@@ -0,0 +1,100 @@
+import datetime as dt
+
+import dateparser
+import scrapy
+
+from gazette.items import Gazette
+from gazette.spiders.base import BaseGazetteSpider
+
+
+class DoemGazetteSpider(BaseGazetteSpider):
+    """
+    Base spider for all cities listed on https://doem.org.br
+    """
+
+    custom_settings = {
+        "DOWNLOAD_FAIL_ON_DATALOSS": False,
+    }
+
+    allowed_domains = ["doem.org.br"]
+    start_date = dt.date(2009, 1, 1)
+
+    def start_requests(self):
+        yield scrapy.Request(self.get_url())
+
+    def parse_pagination(self, response):
+        """
+        This parse function is used to get all the pages available and
+        return request object for each one
+        """
+        return [
+            scrapy.Request(self.get_url(page), callback=self.parse)
+            for page in range(1, 1 + self.get_last_page(response))
+        ]
+
+    def parse(self, response, page=1):
+        """
+        Parse each page from the results page and yield the gazette issues available.
+        """
+        gazette_boxes = response.css("div.box-diario")
+
+        for gazette_box in gazette_boxes:
+            file_url = self.get_pdf_url(gazette_box)
+            date = self.get_gazette_date(gazette_box)
+            edition_number = self.get_edition_number(gazette_box)
+
+            if date > self.end_date:
+                continue
+            elif date < self.start_date:
+                return
+
+            yield Gazette(
+                date=date,
+                file_urls=[file_url],
+                edition_number=edition_number,
+                is_extra_edition=False,
+                power="executive_legislative",
+            )
+
+        last_page = self.get_last_page(response)
+        if page < last_page:
+            yield scrapy.Request(
+                url=self.get_url(page + 1), cb_kwargs={"page": page + 1}
+            )
+
+    def get_url(self, page=1):
+        url = f"https://doem.org.br/{self.state_city_url_part}"
+        start_date = self.start_date.strftime("%Y-%m-%d")
+        end_date = self.end_date.strftime("%Y-%m-%d")
+        return f"{url}/pesquisar?data_inicial={start_date}&data_final={end_date}&page={page}"
+
+    def get_last_page(self, response):
+        """
+        Gets the last page number available in the pages navigation menu
+        """
+        pages = response.css("ul.pagination li a::text").getall()
+        if len(pages) == 0:
+            return 1
+        return max([int(page) for page in pages if page.isnumeric()])
+
+    def get_pdf_url(self, response_item):
+        """
+        Gets the url for the gazette inside one of the 'box-diario' divs
+        """
+        preview_link = response_item.css(
+            "a[title='Baixar Publicação']::attr(href)"
+        ).get()
+        return preview_link.replace("previsualizar", "baixar")
+
+    def get_gazette_date(self, response_item):
+        """
+        Get the date for the gazette inside one of the 'box-diario' divs
+        """
+        date = response_item.css("span.data-diario::text").get().strip()
+        return dateparser.parse(date, languages=["pt"]).date()
+
+    def get_edition_number(self, response_item):
+        """
+        Get the edition number inside one of the 'box-diario' divs
+        """
+        return response_item.css("h2::text").re_first(r"Edição\s+([.\d]+)")
---
+++ backend/data_collection/gazette/spiders/base/dosp.py
@@ -0,0 +1,53 @@
+import base64
+import datetime
+import json
+import re
+
+import scrapy
+from dateutil.rrule import WEEKLY, rrule
+
+from gazette.items import Gazette
+from gazette.spiders.base import BaseGazetteSpider
+
+
+class DospGazetteSpider(BaseGazetteSpider):
+    allowed_domains = ["dosp.com.br", "imprensaoficialmunicipal.com.br"]
+
+    # Must be defined into child classes
+    code = None
+    start_date = None
+
+    def _dosp_request(self, start_date, end_date):
+        for date in rrule(freq=WEEKLY, dtstart=start_date, until=end_date):
+            from_date = date.strftime("%Y-%m-%d")
+            to_date = date + datetime.timedelta(days=6)
+            to_date = to_date.strftime("%Y-%m-%d")
+
+            yield scrapy.Request(
+                f"https://dosp.com.br/api/index.php/dioedata.js/{self.code}/{from_date}/{to_date}?callback=dioe"
+            )
+
+    def start_requests(self):
+        yield from self._dosp_request(self.start_date, self.end_date)
+
+    def parse(self, response):
+        # The response are in a javascript format, then needs some clean up
+        data = json.loads(response.text[6:-2])
+
+        for item in data["data"]:
+            code = item["iddo"]
+            code = str(code).encode("ascii")
+            pdf_code = base64.b64encode(code).decode("ascii")
+            file_url = f"https://dosp.com.br/exibe_do.php?i={pdf_code}"
+            date = datetime.datetime.strptime(item["data"], "%Y-%m-%d").date()
+            raw_edition_number = re.search(r"(\d+)([a-zA-Z]*)", item["edicao_do"])
+            edition_number = raw_edition_number.group(1)
+
+            if self.start_date <= date <= self.end_date:
+                yield Gazette(
+                    date=date,
+                    file_urls=[file_url],
+                    edition_number=edition_number,
+                    power="executive_legislative",
+                    is_extra_edition=raw_edition_number.group(2) != "",
+                )
