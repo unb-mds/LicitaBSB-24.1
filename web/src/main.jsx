1136b37713a9a23840362195fd5fc6b78baa5294
m4rllon <marllonpc33@gmail.com>
2024-05-01 07:12:27 -0300

    web.src.components.card-about-bidding: Estilizando componente e configurando mocks

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -11,7 +11,7 @@ import './styles/global.css'
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Landing  />
+    element: <AboutBidding />
   }
 ])
 
--- web/src/pages/about-bidding/index.jsx
+++ web/src/pages/about-bidding/index.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import Header from "../../components/header";
 import CardAbout from "../../components/card-about";
+import sobreLicitacoes from "../../mocks/sobreLicitacoes";
 
 import style from './style.module.css'
 
 export default function AboutBidding() {
+
+    const propsCardAbout = sobreLicitacoes[0]
+
     return(
         <div className={style.aboutBidding}>
-            {/* <div><Header/></div> */}
-            <CardAbout/>
+            <Header/>
+            <CardAbout 
+                primeiroTitulo={propsCardAbout.primeiroTitulo}
+                segundoTitulo={propsCardAbout.segundoTitulo}
+                primeiroP={propsCardAbout.primeiroP}
+                segundoP={propsCardAbout.segundoP}
+                urlDaFonte={propsCardAbout.urlDaFonte}
+                imagem={propsCardAbout.imagem}/>
         </div>
     )
 
