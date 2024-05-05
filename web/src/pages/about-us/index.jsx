fe10c289facc5244602d491e8e512884a11a76cd
m4rllon <marllonpc33@gmail.com>
2024-05-05 08:17:41 -0300

    web.src.pages.about-us: add identidade visual a pagina Sobre nos #50

--- web/src/pages/about-us/index.jsx
+++ web/src/pages/about-us/index.jsx
@@ -9,11 +9,14 @@ export default function AboutUs(){
     return (
         <div>
             <Header/>
-            <h2>Sobre nós</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam dignissimos maxime eveniet odio unde nostrum temporibus incidunt esse quaerat sit delectus ut, quis soluta assumenda exercitationem? Molestias velit quas delectus?</p>
-            <img src='../../assets/download.jpg' alt='Foto de toda a equipe'/>
-            <h2>Agora, um pouco de cada um</h2>
-            {membros.map(membro => <CardMember key={membro.id} nome={membro.nome} descricao={membro.descricao} github={membro.github} id={membro.id}/>)}
+            <div className={styles.aboutUsContext}>
+                <h2 className={styles.aboutUsTitle}>Sobre nós</h2>
+                <p className={styles.aboutUsP}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam dignissimos maxime eveniet odio unde nostrum temporibus incidunt esse quaerat sit delectus ut, quis soluta assumenda exercitationem? Molestias velit quas delectus?</p>
+                <img className={styles.aboutUsImg} src='../../assets/download.jpg' alt='Foto de toda a equipe'/>
+                <h2 className={styles.aboutUsTitle}>Agora, um pouco de cada um</h2>
+                {membros.map(membro => <CardMember key={membro.id} nome={membro.nome} descricao={membro.descricao} github={membro.github} id={membro.id}/>)}
+            </div>
+        
         </div>
     )
 }
