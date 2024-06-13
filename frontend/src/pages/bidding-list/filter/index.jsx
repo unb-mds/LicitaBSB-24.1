4b62ee03b3f97a6d969348ca51d5f30e823d3433
Maria Helena Carvalho <96885338+MariaCHelena@users.noreply.github.com>
2024-06-13 15:20:11 -0300

    feat: criando filtro de licitacoes #66

---
+++ frontend/src/pages/bidding-list/filter/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './style.module.css';
+
+export default function Filter() {
+  return (
+    <section className={styles.filterSection}>
+      Filter
+    </section>
+  )
+}
