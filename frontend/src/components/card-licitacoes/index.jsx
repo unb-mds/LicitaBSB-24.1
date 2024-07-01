01fe57437c299fc1e65c28e4b34c2612f7b506ea
m4rllon <marllonpc33@gmail.com>
2024-07-01 11:40:18 -0300

    frontend.src.componentes: finalizar a refatoracao do card da licitacao

--- frontend/src/components/card-licitacoes/index.jsx
+++ frontend/src/components/card-licitacoes/index.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
+import { setStatusBidding } from '../../utils/status-bidding'
 
 import styles from './style.module.css'
 
 export default function CardLicitacoes({ data }) {
 
   const formatValue = parseFloat(data["Valor_Licitacao"]).toFixed(2)
+  const statusBidding = setStatusBidding(data)
 
   return (
     <div className={styles.cardWrapper}>
       <h5 className={styles.cardTitle}>{data["Nome_UG"]}</h5>
+      <div className={styles.cardStatus}>
+        <p className={styles.cardStatusText}>Status: {statusBidding}</p>
+        <p className={styles.cardStatusText}>{data['tipo']}</p>
+      </div>
       <div className={styles.licitacoesInfo}>
         <div className={styles.cardSection}>
-          <p>{data["data_abertura"]}</p>
-          <p>{`R$${formatValue}`}</p>
+          <p>Dara de publicação: {data["data_abertura"]}</p>
+          <p>Valor da licitação: {`R$${formatValue}`}</p>
         </div>
         <div>
           <p className={styles.cardDescricao}>{data["Objeto"]}</p>
