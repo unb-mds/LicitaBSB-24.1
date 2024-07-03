bdb64861756f0ff6eeb350e8a5f499c91bf578a1
Maria Helena Carvalho <96885338+MariaCHelena@users.noreply.github.com>
2024-07-03 00:03:25 -0300

    fix: resolvendo problemas de importação de imagens

--- frontend/src/pages/about-us/card-member/index.jsx
+++ frontend/src/pages/about-us/card-member/index.jsx
@@ -1,10 +1,22 @@
+import { useEffect, useState } from 'react';
 import styles from './style.module.css'
 import { FaGithub } from "react-icons/fa";
 
 export default function CardMember({nome, descricao, github, id}){
+    const [imageSrc, setImageSrc] = useState('');
+
+    useEffect(() => {
+    import(`../../../../assets/members/${id}.jpg`)
+      .then(image => {
+        setImageSrc(image.default);
+      })
+      .catch(err => {
+        console.error('Erro ao carregar a imagem:', err);
+      });
+    }, [id])
 
     return(
-        <div 
+        <div
          className={styles.cardAboutUsContext}
          style={{flexDirection: (id%2 === 1 ? 'row' : 'row-reverse')}}>
             <span>
@@ -12,7 +24,7 @@ export default function CardMember({nome, descricao, github, id}){
                 <p className={styles.cardAboutUsDes}>{descricao}</p>
             </span>
             <span className={styles.cardAboutUsPhotoContext}>
-                <img className={styles.cardAboutUsPhoto} src={`../../../../assets/members/${id}.jpg`} alt='Foto do membro'/>
+                <img className={styles.cardAboutUsPhoto} src={imageSrc} alt='Foto do membro'/>
                 <a href={github}>
                     <FaGithub/> Github
                 </a>
--- frontend/src/pages/about-us/index.jsx
+++ frontend/src/pages/about-us/index.jsx
@@ -1,6 +1,7 @@
 import Header from '../../components/header'
 import membros from '../../mocks/members'
 import CardMember from './card-member'
+import Banner from '../../../assets/download.jpg'
 
 import styles from './style.module.css'
 
@@ -12,11 +13,11 @@ export default function AboutUs(){
             <div className={styles.aboutUsContext}>
                 <h2 className={styles.aboutUsTitle}>Sobre nós</h2>
                 <p className={styles.aboutUsP}>Somos um grupo de estudantes da Universidade de Brasília (UnB) dedicados a facilitar o acesso à informação sobre licitações e promover a transparência pública nesse processo. Nosso objetivo é criar ferramentas que simplifiquem o entendimento e a consulta de dados de licitações, contribuindo para uma gestão pública mais transparente e eficiente.</p>
-                <img className={styles.aboutUsImg} src='../../assets/download.jpg' alt='Foto de toda a equipe'/>
+                <img className={styles.aboutUsImg} src={Banner} alt='Foto de toda a equipe'/>
                 <h2 className={styles.aboutUsTitle}>Agora, um pouco de cada um</h2>
-                {membros.map(membro => <CardMember key={membro.id} nome={membro.nome} descricao={membro.descricao} github={membro.github} id={membro.id}/>)}
+                {membros.map(membro => <CardMember key={membro.id} {...membro} />)}
             </div>
-        
+
         </div>
     )
 }
