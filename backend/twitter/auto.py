9ead409b04d66f9be19d2530e114f5bdb5a71ef5
Thales Euflauzino <120137721+thaleseuflauzino@users.noreply.github.com>
2024-07-14 17:10:25 -0300

    Update auto.py

--- backend/twitter/auto.py
+++ backend/twitter/auto.py
@@ -82,7 +82,7 @@ else:
     for licitacao in licitacoes:
         link_encurtado = encurtar_url(licitacao['link'])
         tweet_message = f'{licitacao["titulo"]}\nMais detalhes: {link_encurtado}\n\n{licitacao["descricao"]}'
-        tweet_message = tweet_message.replace("Objeto:", "\nObjeto:")
+        tweet_message = tweet_message.replace("Objeto:", "\n\nObjeto:")
         # Garante que a mensagem não ultrapasse 280 caracteres
         tweet_message = editar_mensagem(tweet_message)
         mensagens.append(tweet_message)
