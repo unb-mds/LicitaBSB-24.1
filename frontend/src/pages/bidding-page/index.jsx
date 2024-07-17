468f9e7b7d132630c4bca6b1fb4a16afb9dd88c3
Maria Helena Carvalho <96885338+MariaCHelena@users.noreply.github.com>
2024-07-17 15:33:14 -0300

    feat: adicionando icones da licitação e corrigindo responsividade

--- frontend/src/pages/bidding-page/index.jsx
+++ frontend/src/pages/bidding-page/index.jsx
@@ -5,6 +5,12 @@ import { useParams } from "react-router-dom";
 import { getLicitacaoById } from "../../services/licitacoes.service";
 import { formatBiddingObject } from "../../utils/format-bidding-object";
 
+import facebook from "../../../assets/facebook.svg";
+import google from "../../../assets/google.svg";
+import twitter from "../../../assets/twitter.svg";
+import calendario from "../../../assets/calendario.svg";
+import valor from "../../../assets/valor.svg";
+
 
 export default function BiddingPage(){
     const parametros = useParams()
@@ -13,23 +19,46 @@ export default function BiddingPage(){
 
     return(
     <div className={styles.biddingContainer}>
+        <h4 className={styles.biddingType}>{dados.tipo}</h4>
         <div className={styles.headingContainer}>
             <div className={styles.titleContainer}>
                 <h3 className={styles.title}>{dados.nomeOrgao}</h3>
                 <span className={styles.subtitle}>{dados.nomeOrgao}</span>
             </div>
-            <div>
-                {/* LINKS PARA COMPARTILHAMENTO */}
+            <div className={styles.shareContainer}>
+                <a href="">
+                    <img src={twitter}/>
+                </a>
+                <a href="">
+                    <img src={facebook}/>
+                </a>
+                <a href="">
+                    <img src={google}/>
+                </a>
             </div>
         </div>
-        <div>
-            <p>{dados.data_abertura}</p>
-            <p>{dados.valor_Licitacao}</p>
+        <div className={styles.biddingInfoContainer}>
+            <div className={styles.biddingInfoElement}>
+                <img src={calendario}/>
+                <p>{dados.data_abertura}</p>
+            </div>
+            {
+                dados.valor_Licitacao && <div className={styles.biddingInfoElement}>
+                    <img src={valor}/>
+                    <p>{dados.valor_Licitacao}</p>
+                </div>
+
+            }
         </div>
 
         <div className={styles.horizontalLine}></div>
 
         <p className={styles.objetoText}>{dados.objeto}</p>
+        <div className={styles.BiddingLink}>
+            {
+                dados.link && <p>Acessar licitação em: <a href={`${dados.link}`} target="blank">{dados.link}</a></p>
+            }
+        </div>
     </div>
     )
 }
