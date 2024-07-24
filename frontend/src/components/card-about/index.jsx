6d42637875cc2144ba44a5a487fe79670283feb4
Maria Helena Carvalho <96885338+MariaCHelena@users.noreply.github.com>
2024-07-24 19:01:53 -0300

    fix: resolvendo conflitos do merge

--- frontend/src/components/card-about/index.jsx
+++ frontend/src/components/card-about/index.jsx
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-
-import style from './style.module.css'
-
-export default function CardAbout({primeiroTitulo, segundoTitulo, primeiroP, segundoP, urlDaFonte, imagem}) {
-    return(
-        <div className={style.cardAboutContext}>
-            <h2 className={style.cardAboutText}>{primeiroTitulo}</h2>
-
-            <p className={style.cardAboutTextP}>{primeiroP}</p>
-
-            <h2 className={style.cardAboutText}>{segundoTitulo}</h2>
-
-            <p className={style.cardAboutTextP}>{segundoP}</p>
-
-            <img className={style.cardAboutImg} src={imagem}/>
-
-            <p className={style.cardAboutTextP}>Para saber mais, acesso este <a target="_blank" href={urlDaFonte}>link</a>.</p>
-        </div>
-    )
-}
-=======
 import React from 'react';
 
 import style from './style.module.css';
@@ -55,4 +32,3 @@ export default function CardAbout({
     </div>
   );
 }
->>>>>>> 4a2ed564d6a18051c82162ac5069cfeb6e4ee553
--- frontend/src/pages/about-bidding/index.jsx
+++ frontend/src/pages/about-bidding/index.jsx
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import Header from "../../components/header";
-import CardAbout from "../../components/card-about";
-import sobreLicitacoes from "../../mocks/sobreLicitacoes";
-
-import style from './style.module.css'
-
-export default function AboutBidding() {
-
-    const propsCardAbout = sobreLicitacoes[0]
-
-    return(
-        <div className={style.aboutBidding}>
-            <CardAbout
-                primeiroTitulo={propsCardAbout.primeiroTitulo}
-                segundoTitulo={propsCardAbout.segundoTitulo}
-                primeiroP={propsCardAbout.primeiroP}
-                segundoP={propsCardAbout.segundoP}
-                urlDaFonte={propsCardAbout.urlDaFonte}
-                imagem={propsCardAbout.imagem}/>
-        </div>
-    )
-
-}
-=======
 import React from 'react';
 import Header from '../../components/header';
 import CardAbout from '../../components/card-about';
@@ -47,4 +21,3 @@ export default function AboutBidding() {
     </div>
   );
 }
->>>>>>> 4a2ed564d6a18051c82162ac5069cfeb6e4ee553
