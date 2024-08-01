6c3e80258abced67614a080735cee249b2814f10
moonshinerd <vitorhls15@gmail.com>
2024-08-01 17:11:28 -0300

    fix: adicionando um novo host

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ygz=9^0q@e1q_yju4*)(cs0_+x3&xcdt7a-#rv_dt4y^!638x#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['licitabsbserver-b85620d9bf48.herokuapp.com']
 
 
 # Application definition
