04ec9f69997432c8275c35319c13e001f6453c9d
m4rllon <marllonpc33@gmail.com>
2024-08-04 12:12:48 -0300

    test: Teste para renderizar a imagem do artigo#110

--- frontend/src/components/card-about/card-about.test.js
+++ frontend/src/components/card-about/card-about.test.js
@@ -63,4 +63,14 @@ describe("Deve renderizar as seguintes informações do artigo: ", () => {
         const info = screen.getByRole("link")
         expect(info).toBeInTheDocument()
     })
+    test("Imagem do artigo", () => {
+        render(<CardAbout primeiroTitulo={artigoTeste.primeiroTitulo}
+            segundoTitulo={artigoTeste.segundoTitulo}
+            primeiroP={artigoTeste.primeiroP}
+            segundoP={artigoTeste.segundoP}
+            urlDaFonte={artigoTeste.urlDaFonte}
+            imagem={artigoTeste.imagem} />)
+        const info = screen.getByRole("img")
+        expect(info).toBeInTheDocument()
+    })
 })
