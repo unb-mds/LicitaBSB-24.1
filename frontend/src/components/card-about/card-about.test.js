db7a06ffdead9b38af1a5b07de0618c6833a81c5
m4rllon <marllonpc33@gmail.com>
2024-08-04 11:57:03 -0300

    test: Adicionando arquivo para testes do componente CardAbout e redigindo mock para props do componente#110

---
+++ frontend/src/components/card-about/card-about.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '@testing-library/jest-dom'
+import { render, screen } from "@testing-library/react";
+import CardAbout from ".";
+
+const artigoTeste = {
+    primeiroTitulo: "Título teste",
+    segundoTitulo: "Subtítulo teste",
+    primeiroP: "conteudoTeste",
+    segundoP: "conteudoTeste",
+    urlDaFonte: "linkTeste",
+    imagem: "urlTeste",
+}
