355da287103dcc68db68b59366ddc806e66d1bf7
m4rllon <marllonpc33@gmail.com>
2024-08-04 11:59:22 -0300

    test: Teste para renderizar o subtitulo do artigo no componente CardAbout#110

--- frontend/src/components/card-about/card-about.test.js
+++ frontend/src/components/card-about/card-about.test.js
@@ -23,5 +23,14 @@ describe("Deve renderizar as seguintes informações do artigo: ", () => {
         const info = screen.getByText(artigoTeste.primeiroTitulo)
         expect(info).toBeInTheDocument("Título teste")
     })
-
+    test("Subtítulo do artigo", () => {
+        render(<CardAbout primeiroTitulo={artigoTeste.primeiroTitulo}
+            segundoTitulo={artigoTeste.segundoTitulo}
+            primeiroP={artigoTeste.primeiroP}
+            segundoP={artigoTeste.segundoP}
+            urlDaFonte={artigoTeste.urlDaFonte}
+            imagem={artigoTeste.imagem} />)
+        const info = screen.getByText(artigoTeste.segundoTitulo)
+        expect(info).toBeInTheDocument("Subtítulo teste")
+    })
 })
