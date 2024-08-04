f584acbba2f472362b770dc30ab03d0a4008993b
m4rllon <marllonpc33@gmail.com>
2024-08-04 10:55:53 -0300

    refactor: refatorando teste para renderizar as seguintes informações da licitação: Tipo e Órgão#122

--- frontend/src/pages/bidding-page/bidding-page.test.js
+++ frontend/src/pages/bidding-page/bidding-page.test.js
@@ -4,13 +4,6 @@ import { MemoryRouter, Route, Routes } from 'react-router-dom'
 
 describe("Deve renderizar as seguintes informações da licitação: ", () => {
     test("tipo da licitação e órgão.", () => {
-        const licitacaoTeste = {
-            tipo: "Tipo Teste",
-            nomeOrgao: "Orgão Teste",
-            data_abertura: "XX/XX/XXXX",
-            valor_Licitacao: "R$ XXX,XX",
-            objeto: "Objeto Teste",
-        }
         const mockId = '10101010'
         render(
             <MemoryRouter initialEntries={[`/licitacoes/${mockId}`]}>
@@ -22,4 +15,5 @@ describe("Deve renderizar as seguintes informações da licitação: ", () => {
         const info = screen.getAllByRole('heading')
         expect(info).toHaveLength(2)
     })
+
 })
