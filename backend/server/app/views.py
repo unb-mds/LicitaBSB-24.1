35fbcee6c3efc32626d778a746a19b6d4435ed35
moonshinerd <vitorhls15@gmail.com>
2024-08-07 08:01:00 -0300

    feat: documentando endpoints com o swagger

--- backend/server/app/views.py
+++ backend/server/app/views.py
@@ -5,10 +5,15 @@ from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
 from rest_framework.pagination import PageNumberPagination
 from datetime import datetime
-from django.core.paginator import Paginator
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 
-
+@swagger_auto_schema(
+    method='get',
+    operation_description="Obter nome do órgão por ID",
+    responses={200: OrgaoSerializer()}
+)
 @api_view(['GET'])
 def nome_orgaos_por_id(request, id):
     try:
@@ -20,72 +25,76 @@ def nome_orgaos_por_id(request, id):
     return Response(serializer.data)
 
 
+@swagger_auto_schema(
+    method='get',
+    operation_description="Listar todos os órgãos com opção de pesquisa e paginação",
+    manual_parameters=[
+        openapi.Parameter('search', openapi.IN_QUERY, description="Termo de pesquisa para nome do órgão", type=openapi.TYPE_STRING)
+    ],
+    responses={200: OrgaoSerializer(many=True)}
+)
 @api_view(['GET'])
 def listar_orgaos(request):
-    # Define o número de itens por página
     paginator = PageNumberPagination()
     paginator.page_size = 10
 
-    # Obtém o termo de pesquisa do parâmetro 'search'
     search_term = request.GET.get('search', '')
 
-    # Filtra órgãos pelo nome, usando o termo de pesquisa
     if search_term:
         orgaos = Orgao.objects.filter(nome__icontains=search_term)
     else:
         orgaos = Orgao.objects.all()
 
-    # Paginação
     page = paginator.paginate_queryset(orgaos, request)
     serializer = OrgaoSerializer(page, many=True)
-
-    # Prepara a resposta com a paginação
     return paginator.get_paginated_response(serializer.data)
 
 
-
+@swagger_auto_schema(
+    method='get',
+    operation_description="Listar todas as licitações com filtros dinâmicos e paginação",
+    manual_parameters=[
+        openapi.Parameter('tipo', openapi.IN_QUERY, description="Tipo de licitação", type=openapi.TYPE_STRING),
+        openapi.Parameter('data', openapi.IN_QUERY, description="Data da licitação (dd-mm-aaaa)", type=openapi.TYPE_STRING)
+    ],
+    responses={200: LicitacaoSerializer(many=True)}
+)
 @api_view(['GET'])
 def listar_licitacoes(request):
     paginator = PageNumberPagination()
-    paginator.page_size = 10  # Define o número de itens por página
+    paginator.page_size = 10
 
-    # Lista de possíveis campos de filtro
     filtro_campos = ['tipo', 'data']
-
-    # Dicionário para armazenar os filtros dinâmicos
     filtros = {}
 
-    # Itera sobre os campos de filtro possíveis e adiciona ao dicionário de filtros
     for campo in filtro_campos:
         valor = request.GET.get(campo)
         if valor:
             if campo == 'data':
-                # Converte o formato da data de dd-mm-aaaa para dd/mm/aaaa
                 valor = valor.replace('-', '/')
             filtros[campo] = valor
-            print(f"Filtrando por {campo}: {valor}")  # Mensagem para verificação
 
     licitacoes = Licitacao.objects.filter(**filtros)
 
-    if not licitacoes.exists():
-        print(f"Nenhuma licitação encontrada com os filtros: {filtros}")  # Mensagem quando não encontrar licitações
-    else:
-        print(f"{licitacoes.count()} licitações encontradas com os filtros: {filtros}")
-
-    # Ordena as licitações por data mais recente
-    licitacoes = sorted(licitacoes, key=lambda x: datetime.strptime(x.data, '%d/%m/%Y'), reverse=True)
+    if licitacoes.exists():
+        licitacoes = sorted(licitacoes, key=lambda x: datetime.strptime(x.data, '%d/%m/%Y'), reverse=True)
 
     result_page = paginator.paginate_queryset(licitacoes, request)
     serializer = LicitacaoSerializer(result_page, many=True)
     return paginator.get_paginated_response(serializer.data)
-    
 
+
+@swagger_auto_schema(
+    method='get',
+    operation_description="Obter detalhes de uma licitação por ID",
+    responses={200: LicitacaoSerializer()}
+)
 @api_view(['GET'])
 def licitacao_por_id(request, id):
     try:
         licitacao = Licitacao.objects.get(pk=id)
     except Licitacao.DoesNotExist:
         raise NotFound("Licitacao não encontrada")
-    
+
     serializer = LicitacaoSerializer(licitacao)
     return Response(serializer.data)
