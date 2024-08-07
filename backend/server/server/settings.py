ee6068dd3a0b6ab20d56d7092f9e34e4dffc9645
Víctor Schmidt <121871105+moonshinerd@users.noreply.github.com>
2024-08-07 18:48:15 -0300

    Update settings.py

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ygz=9^0q@e1q_yju4*)(cs0_+x3&xcdt7a-#rv_dt4y^!638x#
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'licitabsbserver-6f0bfb6e0572.herokuapp.com',
+    'licitabsbserer-a1c309841042.herokuapp.com',
     'localhost'
     ]
 
