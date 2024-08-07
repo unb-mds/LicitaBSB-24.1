7d97e81bb969dc68d334d4c561ccb40095759b19
m4rllon <marllonpc33@gmail.com>
2024-08-07 17:33:07 -0300

    test: Adicionar teste para função 'format-currency' com uma string como entrada de número float com ponto

--- frontend/src/utils/format-currency.test.js
+++ frontend/src/utils/format-currency.test.js
@@ -3,8 +3,13 @@ import { screen, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 describe("Quando a função 'formatCurrency' receber ", () => {
-    test('um valor em string, deve retornar uma string formatada com vírgula', () => {
+    test('um valor uma string de valor inteiro, deve retornar uma string formatada com vírgula', () => {
         const valor = '100'
+        const novoValor = formatCurrency(valor)
+        expect(novoValor).toEqual('100,00')
+    })
+    test('um valor uma string de valor float com ponto, deve retornar uma string formatada com vírgula', () => {
+        const valor = '100.00'
         const novoValor = formatCurrency(valor)
         expect(novoValor).toEqual('100,00')
     })
