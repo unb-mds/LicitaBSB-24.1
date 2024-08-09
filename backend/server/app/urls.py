00286970e416fbbd630ca4693aadde88f0734e52
moonshinerd <vitorhls15@gmail.com>
2024-08-09 13:27:42 -0300

    feat: criando endpoint para quantidade de licitações anuale outro para quantidade de licitações mensal

--- backend/server/app/urls.py
+++ backend/server/app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from app.views import  listar_licitacoes,licitacao_por_id,nome_orgaos_por_id, listar_orgaos, listar_licitacoes_quantidade, listar_licitacoes_valores_mensais, listar_licitacoes_valores_anuais
+from app.views import  listar_licitacoes,licitacao_por_id,nome_orgaos_por_id, listar_orgaos, listar_licitacoes_quantidade_mensal, listar_licitacoes_quantidade_anual, listar_licitacoes_valores_mensais, listar_licitacoes_valores_anuais
 
 
 urlpatterns = [
@@ -7,7 +7,8 @@ urlpatterns = [
     path('orgaos', listar_orgaos, name='lista_orgaos'),
     path('licitacoes', listar_licitacoes, name='listar_licitacoes'),
     path('licitacoes/<int:id>', licitacao_por_id, name='licitacao_por_id'),
-    path('dash/quantidade', listar_licitacoes_quantidade, name= 'listar_licitacoes_quantidade'),
+    path('dash/quantidade-mensal', listar_licitacoes_quantidade_mensal, name= 'listar_licitacoes_quantidade_mensal'),
+    path('dash/quantidade-anual', listar_licitacoes_quantidade_anual, name= 'listar_licitacoes_quantidade_anual'),
     path('dash/valores-mensais', listar_licitacoes_valores_mensais, name='licitacoes-valores-mensais'),
     path('dash/valores-anuais', listar_licitacoes_valores_anuais, name='valores-anuais-list'),
 ]
