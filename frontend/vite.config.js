d9d314c16fccef79bdb491da08759546dc230b37
Víctor Schmidt <121871105+moonshinerd@users.noreply.github.com>
2024-08-11 11:16:41 -0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'frontend/dist',
+  },
+  base: '/',
   plugins: [react()],
 });
-
