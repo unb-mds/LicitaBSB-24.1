e3175cb6fd83c01cae6e40e8efb07b1f0ede2993
Thales Euflauzino <thaleseuflauzino@gmail.com>
2024-08-11 19:55:55 -0300

    :test_tube: test: adicionando primeiro teste de requisição

--- backend/server/app/tests.py
+++ backend/server/app/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
+from rest_framework import status
+from .models import Orgao
 
-# Create your tests here.
+class URLTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.orgao = Orgao.objects.create(id=1, nome='Orgao Teste')
+
+    def test_nome_orgaos_por_id(self):
+        response = self.client.get(reverse('nome_orgaos_por_id', args=[self.orgao.id]))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {'id': self.orgao.id, 'nome': self.orgao.nome})
