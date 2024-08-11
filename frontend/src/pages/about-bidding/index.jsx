ebb0bcfbaf6e9b4ae31b18c12012469f58286b86
m4rllon <marllonpc33@gmail.com>
2024-08-11 12:04:00 -0300

    refactor: Corriginddo resposividade das páginas Sobre o Bot, Sobre as Licitações, Sobre Dispensas#135

--- frontend/src/pages/about-bidding/index.jsx
+++ frontend/src/pages/about-bidding/index.jsx
@@ -1,23 +1,6 @@
 import React from 'react';
-import Header from '../../components/header';
-import CardAbout from '../../components/card-about';
-import sobreLicitacoes from '../../mocks/sobreLicitacoes';
-
 import style from './style.module.css';
 
 export default function AboutBidding() {
-  const propsCardAbout = sobreLicitacoes[0];
-
-  return (
-    <div className={style.aboutBidding}>
-      <CardAbout
-        primeiroTitulo={propsCardAbout.primeiroTitulo}
-        segundoTitulo={propsCardAbout.segundoTitulo}
-        primeiroP={propsCardAbout.primeiroP}
-        segundoP={propsCardAbout.segundoP}
-        urlDaFonte={propsCardAbout.urlDaFonte}
-        imagem={propsCardAbout.imagem}
-      />
-    </div>
-  );
+  return <main className={style.mainContext}></main>;
 }
