958ad86f1f3557f3a20f06ce759f3e35492f40c1
Maria Helena Carvalho <96885338+MariaCHelena@users.noreply.github.com>
2024-08-12 18:26:23 -0300

    feat: refatorando rotas e criando rota de artigos

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,46 +1,74 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom';
+
 import Landing from './pages/landing';
 import AboutBidding from './pages/about-bidding';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './styles/global.css';
 import AboutUs from './pages/about-us';
-import Dashboard from './pages/dashboard';
 import BiddingList from './pages/bidding-list';
 import BiddingPage from './pages/bidding-page';
-import Header from './components/header';
-import Footer from './components/footer';
-import { Error } from './pages/error';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import AboutBot from './pages/about-bot';
 import AboutBiddingDispatch from './pages/about-bidding-dispatch';
+import Root from './root';
 
-function AppRoutes() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="*" element={<Error />} />
-        <Route path="/sobrelicitacao" element={<AboutBidding />} />
-        <Route
-          path="/sobredispensadelicitacao"
-          element={<AboutBiddingDispatch />}
-        />
-        <Route path="/sobrebot" element={<AboutBot />} />
-        <Route path="/sobrenos" element={<AboutUs />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/licitacoes" element={<BiddingList />} />
-        <Route path="/licitacoes/:id" element={<BiddingPage />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  );
-}
+import './styles/global.css';
+import ArticleRoot from './article-root';
+import Articles from './pages/articles';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <Landing />
+      },
+      {
+        path: "artigos",
+        element: <ArticleRoot />,
+        children: [
+          {
+            index: true,
+            element: <Articles />
+          },
+          {
+            path: "sobrelicitacao",
+            element: <AboutBidding />
+          },
+          {
+            path: "sobredispensadelicitacao",
+            element: <AboutBiddingDispatch />
+          },
+          {
+            path: "sobrebot",
+            element: <AboutBot />
+          },
+        ]
+      },
+      {
+        path: "sobrenos",
+        element: <AboutUs />
+      },
+      {
+        path: "licitacoes",
+        element: <BiddingList />
+      },
+      {
+        path: "licitacoes/:id",
+        element: <BiddingPage />
+      }
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <LocalizationProvider dateAdapter={AdapterDayjs}>
-    <AppRoutes />
+    <RouterProvider router={router} />
   </LocalizationProvider>,
 );
