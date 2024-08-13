3b1511fc695c2fc2fe59b61c4321ce67ceeb0685
Otavio Brito <otavio.hmbrito@uol.com.br>
2024-08-13 19:50:56 -0300

    style: Aplicar estilização ao componente newsletter

--- frontend/src/components/newsletter/index.jsx
+++ frontend/src/components/newsletter/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import style from '../newsletter/style.module.css'
+import amicoImage from './amico.png';
 
 const SubscribeForm = () => {
   const [email, setEmail] = useState('');
@@ -23,17 +24,24 @@ const SubscribeForm = () => {
   return (
     <form onSubmit={handleSubmit}>
       <div className={style.newsletter}>
-        <h1>Se inscreva para receber atualizações sobre futuras
-          licitações!
-        </h1>
-        <label>Email:</label>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      <button type="submit">Subscribe</button>
+      <div className={style['newsletter-text']}>
+            <h1>Receba as mais recentes licitações diretamente no seu e-mail</h1>
+            <h2>Se você deseja receber no seu melhor email XXXMENTE as licitações mais recentes do Distrito Federal, cadastre seu email no LicitaBSB abaixo:</h2>
+        </div>
+        <div className={style['newsletter-image']}>
+            <img src={amicoImage}/>
+        </div>
+        <div className={style['newsletter-form']}>
+            <input
+                placeholder="Digite seu email aqui."
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+            />
+            <button type="submit">Enviar</button>
+        </div>
+        
       </div>
       {message && <p>{message}</p>}
     </form>
