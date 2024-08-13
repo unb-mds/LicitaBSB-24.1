4640ce3ed787629dc107c5580998d786e3e6f50f
m4rllon <marllonpc33@gmail.com>
2024-08-13 19:54:43 -0300

    fix: Corrigindo erro de rota para o dashboard

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider
-} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import Landing from './pages/landing';
 import AboutBidding from './pages/about-bidding';
@@ -15,6 +12,7 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import AboutBot from './pages/about-bot';
 import AboutLicitaBSB from './pages/about-licitaBSB';
 import AboutBiddingDispatch from './pages/about-bidding-dispatch';
+import Dashboard from './pages/dashboard';
 import Root from './root';
 
 import './styles/global.css';
@@ -23,53 +21,57 @@ import Articles from './pages/articles';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Root />,
     children: [
       {
         index: true,
-        element: <Landing />
+        element: <Landing />,
       },
       {
-        path: "artigos",
+        path: 'artigos',
         element: <ArticleRoot />,
         children: [
           {
             index: true,
-            element: <Articles />
+            element: <Articles />,
           },
           {
-            path: "sobrelicitacao",
-            element: <AboutBidding />
+            path: 'sobrelicitacao',
+            element: <AboutBidding />,
           },
           {
-            path: "sobredispensadelicitacao",
-            element: <AboutBiddingDispatch />
+            path: 'sobredispensadelicitacao',
+            element: <AboutBiddingDispatch />,
           },
           {
-            path: "sobrebot",
-            element: <AboutBot />
+            path: 'sobrebot',
+            element: <AboutBot />,
           },
           {
-            path: "sobrelicitabsb",
-            element: <AboutLicitaBSB />
-          }
-        ]
+            path: 'sobrelicitabsb',
+            element: <AboutLicitaBSB />,
+          },
+        ],
       },
       {
-        path: "sobrenos",
-        element: <AboutUs />
+        path: 'sobrenos',
+        element: <AboutUs />,
       },
       {
-        path: "licitacoes",
-        element: <BiddingList />
+        path: 'licitacoes',
+        element: <BiddingList />,
       },
       {
-        path: "licitacoes/:id",
-        element: <BiddingPage />
-      }
-    ]
-  }
+        path: 'licitacoes/:id',
+        element: <BiddingPage />,
+      },
+      {
+        path: 'dashboard',
+        element: <Dashboard />,
+      },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
