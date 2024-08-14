5c570bd4f4212d06db64e3651c40268017424a03
m4rllon <marllonpc33@gmail.com>
2024-08-14 08:03:39 -0300

    feat: Implementando carrossel de artigos na home page#156

---
+++ frontend/src/mocks/carrossel.js
@@ -0,0 +1,28 @@
+const items = [
+    {
+        id: 1,
+        title: 'Sobre o projeto',
+        paragraph: 'O Licita BSB é um portal online inovador que centraliza as licitações realizadas em Brasília, oferecendo uma plataforma onde qualquer pessoa pode acessar informações detalhadas sobre essas oportunidades.',
+        path: '/artigos/sobrelicitabsb'
+    },
+    {
+        id: 2,
+        title: 'O que é uma licitação?',
+        paragraph: 'É o processo por meio do qual a Administração Pública contrata obras, serviços, compras e alienações. Em outras palavras, licitação é a forma como a Administração Pública pode comprar e vender.',
+        path: '/artigos/sobrelicitacao'
+    },
+    {
+        id: 3,
+        title: 'O que é uma dispensa de licitação?',
+        paragraph: 'A licitação é um processo administrativo formal que visa garantir a isonomia e a melhor proposta para a administração pública na contratação de bens e serviços.',
+        path: '/artigos/sobredispensadelicitacao'
+    },
+    {
+        id: 4,
+        title: 'E o bot?',
+        paragraph: 'Um "bot de Twitter" é um software automatizado que interage com a plataforma X (Twitter). Esses bots podem realizar diversas funções automaticamente, como: Postar tweets, Retweetar, Seguir contas, Responder a tweets e Enviar mensagens diretas.',
+        path: '/artigos/sobrebot'
+    },
+]
+
+export default items
--- frontend/src/pages/landing/index.jsx
+++ frontend/src/pages/landing/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import UltimasLicitacoes from './ultimas-licitacoes';
 import Newsletter from '../../components/newsletter/index.jsx';
+import Carrossel from './carrossel/index.jsx';
+import items from '../../mocks/carrossel.js';
 
 export default function Landing() {
   return (
     <>
       <main>
         <div>
+          <Carrossel items={items} />
           <UltimasLicitacoes />
           <Newsletter />
         </div>
