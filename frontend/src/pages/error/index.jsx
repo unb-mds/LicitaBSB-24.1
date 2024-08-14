ee6132dfdcd615140c8ff8789c8798a0291aa74b
Maria Helena Carvalho <96885338+MariaCHelena@users.noreply.github.com>
2024-08-13 21:32:03 -0300

    feat: estilizando página de erro

--- frontend/src/pages/error/index.jsx
+++ frontend/src/pages/error/index.jsx
@@ -1,6 +1,6 @@
 export default function Error() {
   return (
-    <main>
+    <main style={{display: 'flex', alignItems: 'center', justifyContent: 'center', flex: 1, flexDirection: 'column'}}>
       <h1>Não há nada aqui...</h1>
       <p>Ocorreu um erro, tente novamente!</p>
     </main>
